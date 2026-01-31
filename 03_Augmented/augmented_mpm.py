9a705507725fb52790a86802da4b5783b0312ebe
3n3l <nncnep@gmail.com>
2026-01-31 14:19:31 +0100

    allow for more hardening

--- 03_Augmented/augmented_mpm.py
+++ 03_Augmented/augmented_mpm.py
@@ -148,7 +148,7 @@ class AugmentedMPM(StaggeredSolver):
             # Apply ice hardening by adjusting Lame parameters:
             la, mu = self.lambda_p[p], self.mu_p[p]
             if self.phase_p[p] == Ice.Phase:
-                hardening = ti.max(0.1, ti.min(20, ti.exp(self.zeta_p[p] * (1.0 - self.JP_p[p]))))
+                hardening = ti.max(0.1, ti.min(50, ti.exp(self.zeta_p[p] * (1.0 - self.JP_p[p]))))
                 la, mu = la * hardening, mu * hardening
 
             # Eliminate dilational component explicitly [Jiang 2014, Eqn. 8], then
